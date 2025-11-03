9d990df901be128420765bd0d69f116efc4553ee
Harshadsshinde <harshads1433@gmail.com>
2025-11-03 23:57:31 +0530

    first commit

--- App.py
+++ App.py
@@ -17,6 +17,17 @@ def mytask():
 @app.route('/papercheck', methods=['GET','POST'])
 def papercheck():
     return render_template('paperChecking.html');
+@app.route('/checkedpaper', methods=['GET','POST'])
+def checkedpaper():
+    return render_template('checkedPaper.html');
+
+@app.route('/analysis', methods=['GET','POST'])
+def analysis():
+    return render_template('analysis.html');
+
+@app.route('/resources', methods=['GET','POST'])
+def resources():
+    return render_template('resources.html');
 
 app.run(debug=True)
 
